033deedcb63e85a68d17881a02827b475abf8118
shermatovelbek <shermatovelbek772@gmail.com>
2023-12-01 21:02:48 +0500

    misollarni ishlashga tayyor

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let a = 2;
+let b = 25;
+let c = 30;
+
+let counter = 0;
+let sum = 0;
+
+ if(a > 0 ){
+    counter += a;
+    sum++;
+ }
+
+ if(b > 0 ){
+    counter += b;
+    sum++;
+ }
+
+ if(c > 0 ){
+    counter += c;
+    sum++;
+ }
+
+ sum = (a + b + c) / 3;
+
+ console.log(sum);
+//  console.log(counter);
